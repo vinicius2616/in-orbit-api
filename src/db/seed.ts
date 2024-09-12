bc5b74164daf555065808d2030e9dfe4640a1ee1
Vinicius Faleiros <vinicius.faleiros@hotmail.com>
2024-09-12 13:54:09 -0300

    continue dev

---
+++ src/db/seed.ts
@@ -0,0 +1,26 @@
+import { client, db } from ".";
+import { goalCompletions, goals } from "./schema";
+
+async function seed() {
+  await db.delete(goalCompletions);
+  await db.delete(goals);
+
+  await db.insert(goals).values([
+    {
+      title: "Acordar cedo",
+      desiredWeeklyFrequency: 5,
+    },
+    {
+      title: "Academia",
+      desiredWeeklyFrequency: 3,
+    },
+    {
+      title: "Meditar",
+      desiredWeeklyFrequency: 1,
+    },
+  ]);
+}
+
+seed().finally(() => {
+  client.end();
+});
