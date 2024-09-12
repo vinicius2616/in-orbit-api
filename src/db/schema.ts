0390eed8a0307cbaccf9a29c54b16b6d187d0c38
Vinicius Faleiros <vinicius.faleiros@hotmail.com>
2024-09-11 23:29:29 -0300

    init commit

---
+++ src/db/schema.ts
@@ -0,0 +1,8 @@
+import { integer, pgTable, text, timestamp } from "drizzle-orm/pg-core";
+
+export const goals = pgTable("goals", {
+  id: text("id").primaryKey(),
+  title: text("title").notNull(),
+  desiredWeeklyFrequency: integer("desired_weekly_frequency").notNull(),
+  createdAt: timestamp("created_at", { withTimezone: true }).defaultNow(),
+});
